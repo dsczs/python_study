3aabab94a75358bb2b35fd361d426a0e8022dc75
章刚 <37063594@qq.com>
2020-01-06 18:57:54 +0800

    python 类

--- pythondemo/Student.py
+++ pythondemo/Student.py
@@ -2,15 +2,18 @@
 
 
 class Student:
-    # 全局变量
+    # 全局变量 public static
     public_var = 22
-    # 局部变量 不可直接引用
+    # 局部变量 不可直接引用 private
     __name = ''
     __age = 0
+    # 局部变量 protected
+    _score = 0
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, score):
         self.__name = name
         self.__age = age
+        self._score = score
 
     def test_hello(self):
         print("this.name = ", self.__name, "this.age = ", self.__age)
--- pythondemo/Test4.py
+++ pythondemo/Test4.py
@@ -3,7 +3,14 @@ from pythondemo.Student import Student
 
 
 if __name__ == '__main__':
-    student = Student('hello', 33)
+    student = Student('hello', 33, 95)
     # student.test_hello()
     print(student)
+
+    # 注意这里不可访问私有的成员变量 但是可以使用这种方式访问 name是private的
+    print(student._Student__name)
+
+    # score是protected可以直接通过实例访问
+    print(student._score)
+
     print(Student.public_var)
