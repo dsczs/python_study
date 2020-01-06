14bd9e859a29dfba35421479ec28e1b104186bf7
章刚 <37063594@qq.com>
2020-01-06 18:50:05 +0800

    python 类

--- pythondemo/Student.py
+++ pythondemo/Student.py
@@ -2,13 +2,19 @@
 
 
 class Student:
+    # 全局变量
+    public_var = 22
+    # 局部变量 不可直接引用
+    __name = ''
+    __age = 0
 
-    name = ''
-    age = 0
-
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
+    def __init__(self, name, age):
+        self.__name = name
+        self.__age = age
 
     def test_hello(self):
-        print("this.name = ", self.name, "this.age = ", self.age)
+        print("this.name = ", self.__name, "this.age = ", self.__age)
+
+    def __str__(self) -> str:
+        return "this.name = " + self.__name + " this.age = " + str(self.__age)
+
--- pythondemo/Test4.py
+++ pythondemo/Test4.py
@@ -4,4 +4,6 @@ from pythondemo.Student import Student
 
 if __name__ == '__main__':
     student = Student('hello', 33)
-    student.test_hello()
+    # student.test_hello()
+    print(student)
+    print(Student.public_var)
