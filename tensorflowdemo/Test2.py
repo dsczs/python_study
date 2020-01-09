5cc524c5e5b09790608e42782d9bb32fcedf9eb8
章刚 <37063594@qq.com>
2020-01-09 14:02:12 +0800

    new session this method don't need to close session by you

--- tensorflowdemo/Test2.py
+++ tensorflowdemo/Test2.py
@@ -17,6 +17,18 @@ def test_tf_1():
     sess.close()
 
 
+# new session this method don't need to close session by you
+def test_tf_2():
+    matrix1 = tf.constant([[3., 3.]])
+    matrix2 = tf.constant(2., shape=(2, 2))
+
+    product = tf.matmul(matrix1, matrix2)
+
+    with tf.Session() as sess:
+        result = sess.run(product)
+        print(result)
+
+
 if __name__ == '__main__':
-    test_tf_1()
+    test_tf_2()
 
